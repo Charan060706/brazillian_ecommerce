1b5e58f94b1e4ad6f11cafb67f089cdb8f6deedc
Charan060706 <cs23b011@iittp.ac.in>
2026-08-05 10:50:42 +0530

    re structuring-1

--- olist_analytics/models/staging/stg_products.sql
+++ olist_analytics/models/staging/stg_products.sql
@@ -1,4 +1,11 @@
-with source as (
-    select * from {{source('olist','raw_products')}}
-)
-select * from source
+select
+    product_id,
+    coalesce(product_category_name, 'unknown') as product_category_name,
+    product_name_lenght,
+    product_description_lenght,
+    product_photos_qty,
+    product_weight_g,
+    product_length_cm,
+    product_height_cm,
+    product_width_cm
+from {{ source('olist', 'raw_products') }}
